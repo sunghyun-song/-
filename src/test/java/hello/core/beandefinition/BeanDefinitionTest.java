c084cbaee31829519fd875620a9c30c89617ee31
송성현 <songshyun1@gsretail.com>
2023-10-24 19:29:13 +0900

    스프링 빈 설정 메타 정보 - BeanDefinition

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -4,11 +4,15 @@ import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.web.reactive.context.GenericReactiveWebApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    //GenericXmlApplicationContext ac = new GenericXmlApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
